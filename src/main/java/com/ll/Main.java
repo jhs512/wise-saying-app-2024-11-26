1173868360aef0157ea2f41b2e81b12495716845
jhs512 <jangka512@gmail.com>
2024-11-26 12:39:09 +0900

    work

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -7,7 +7,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // lab1();
-        lab2();
+        // lab2();
+
+        lab3();
     }
 
     private static void lab1() {
@@ -32,5 +34,14 @@ public class Main {
         cmd = scanner.nextLine().trim();
 
         System.out.println("입력한 명령: " + cmd);
+    }
+
+    private static void lab3() {
+        System.out.println("== 시작 ==");
+
+        System.out.println("안녕하세요.");
+        System.out.println("반갑습니다.");
+
+        System.out.println("== 끝 ==");
     }
 }
