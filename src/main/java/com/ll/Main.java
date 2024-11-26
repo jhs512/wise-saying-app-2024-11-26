8c2eeb6bb8d376d7161111426513dc93f12ee01c
jhs512 <jangka512@gmail.com>
2024-11-26 12:29:15 +0900

    work

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,10 +1,13 @@
 package com.ll;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        lab1();
+        // lab1();
+        lab2();
     }
 
     private static void lab1() {
@@ -12,6 +15,21 @@ public class Main {
 
         System.out.print("명령) ");
         String cmd = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령: " + cmd);
+    }
+
+    private static void lab2() {
+        InputStream in = new ByteArrayInputStream("안녕\n잘가".getBytes());
+        Scanner scanner = new Scanner(in);
+
+        System.out.println("명령) ");
+        String cmd = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령: " + cmd);
+
+        System.out.println("명령) ");
+        cmd = scanner.nextLine().trim();
 
         System.out.println("입력한 명령: " + cmd);
     }
