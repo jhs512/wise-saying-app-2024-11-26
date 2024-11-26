4635edda65687f25865153dd9007c877e96eaa91
jhs512 <jangka512@gmail.com>
2024-11-26 14:29:05 +0900

    work

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -1,12 +1,8 @@
 package com.ll.domain.wiseSaying.controller;
 
-import com.ll.App.App;
-import com.ll.standard.util.TestUtil;
+import com.ll.AppTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,15 +11,7 @@ public class WiseSayingControllerTest {
     @Test
     @DisplayName("== 명언 앱 ==")
     public void t1() {
-        Scanner scanner = TestUtil.getScanner("종료");
-        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
-
-        App app = new App(scanner);
-        app.run();
-
-        String output = outputStream.toString();
-
-        TestUtil.clearSetOutToByteArray(outputStream);
+        String output = AppTest.run("종료");
 
         assertThat(output)
                 .contains("== 명언 앱 ==");
@@ -32,20 +20,13 @@ public class WiseSayingControllerTest {
     @Test
     @DisplayName("명령) ")
     public void t2() {
-        Scanner scanner = TestUtil.getScanner("""
+        String output = AppTest.run("""
                 목록
                 종료
                 """);
-        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
-
-        App app = new App(scanner);
-        app.run();
-
-        String output = outputStream.toString();
-
-        TestUtil.clearSetOutToByteArray(outputStream);
 
         assertThat(output)
                 .contains("명령) ");
     }
 }
+
