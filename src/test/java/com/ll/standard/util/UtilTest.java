73d21578667b976ba03278c5498ee2ae5ddade28
jhs512 <jangka512@gmail.com>
2024-11-27 11:37:56 +0900

    f1 g

--- src/test/java/com/ll/standard/util/UtilTest.java
+++ src/test/java/com/ll/standard/util/UtilTest.java
@@ -3,6 +3,8 @@ package com.ll.standard.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UtilTest {
     @Test
     @DisplayName("파일을 생성할 수 있다.")
