bbe7e5f9ac73a65fbd678b0f7a6dacf6d42608b2
jhs512 <jangka512@gmail.com>
2024-11-27 14:18:04 +0900

    f1 r

---
+++ src/test/java/com/ll/standard/util/JsonUtilTest.java
@@ -0,0 +1,29 @@
+package com.ll.standard.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JsonUtilTest {
+    @Test
+    @DisplayName("맵을 JSON으로 바꿀 수 있다.")
+    public void t1() {
+        // given
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "이름");
+
+        // when
+        String jsonStr = Util.json.toString(map);
+
+        // then
+        assertThat(jsonStr).isEqualTo("""
+                {
+                    "name": "이름"
+                }
+                """.stripIndent());
+    }
+}
