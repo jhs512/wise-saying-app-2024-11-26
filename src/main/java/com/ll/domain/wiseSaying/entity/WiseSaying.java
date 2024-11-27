9d661d4edfa8089ff9c4e039fd0ebccd3f0643fd
jhs512 <jangka512@gmail.com>
2024-11-27 14:07:53 +0900

    @AllArgsConstructor

--- src/main/java/com/ll/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/ll/domain/wiseSaying/entity/WiseSaying.java
@@ -1,20 +1,16 @@
 package com.ll.domain.wiseSaying.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+@AllArgsConstructor
 @Getter
 @Setter
 public class WiseSaying {
     private int id;
     private String content;
     private String author;
-
-    public WiseSaying(int id, String content, String author) {
-        this.id = id;
-        this.content = content;
-        this.author = author;
-    }
 
     public boolean isNew() {
         return id == 0;
