a4f2135a846a84085357ed65f58da663aa15a44e
jhs512 <jangka512@gmail.com>
2024-11-27 11:31:52 +0900

    f1 r

---
+++ src/test/java/com/ll/standard/util/UtilTest.java
@@ -0,0 +1,21 @@
+package com.ll.standard.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UtilTest {
+    @Test
+    @DisplayName("파일을 생성할 수 있다.")
+    public void t1() {
+        // given
+        String filePath = "test.txt";
+
+        // when
+        Util.file.touch(filePath);
+
+        // then
+        assertThat(
+                Util.file.exists(filePath)
+        ).isTrue();
+    }
+}
