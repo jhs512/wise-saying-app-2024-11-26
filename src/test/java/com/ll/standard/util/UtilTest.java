033f40710fe2e2023189df335e8bb43a3d6c7aaf
jhs512 <jangka512@gmail.com>
2024-11-27 12:26:04 +0900

    work

--- src/test/java/com/ll/standard/util/UtilTest.java
+++ src/test/java/com/ll/standard/util/UtilTest.java
@@ -67,4 +67,21 @@ public class UtilTest {
                 Util.file.notExists(filePath)
         ).isTrue();
     }
+
+    @Test
+    @DisplayName("파일을 생성할 수 있다, 만약 해당 경로의 폴더가 없다면 만든다.")
+    public void t4() {
+        // given
+        String filePath = "temp/temp/test.txt";
+
+        // when
+        Util.file.touch(filePath);
+
+        // then
+        assertThat(
+                Util.file.exists(filePath)
+        ).isTrue();
+
+        Util.file.delete(filePath);
+    }
 }
