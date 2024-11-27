aa9bfd8adc832f89fa98f0165b679f68672f9297
jhs512 <jangka512@gmail.com>
2024-11-27 15:06:05 +0900

    f4 r/g

--- src/test/java/com/ll/standard/util/JsonUtilTest.java
+++ src/test/java/com/ll/standard/util/JsonUtilTest.java
@@ -179,4 +179,28 @@ public class JsonUtilTest {
                 .containsEntry("name", "이름")
                 .containsEntry("gender", "남자");
     }
+
+    @Test
+    @DisplayName("JSON to Map(숫자필드(실수))")
+    public void t9() {
+        // given
+        String jsonStr = """
+                {
+                    "id": 1,
+                    "name": "이름",
+                    "gender": "남자",
+                    "height": 178.1543221
+                }
+                """.stripIndent().trim();
+
+        // when
+        Map<String, Object> map = Util.json.toMap(jsonStr);
+
+        // then
+        assertThat(map)
+                .containsEntry("id", 1)
+                .containsEntry("name", "이름")
+                .containsEntry("gender", "남자")
+                .containsEntry("height", 178.1543221);
+    }
 }
