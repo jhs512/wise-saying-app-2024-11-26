5414fed36e7a463dba88467522ec15d09eb9e4cf
jhs512 <jangka512@gmail.com>
2024-11-27 14:25:36 +0900

    f2 r

--- src/test/java/com/ll/standard/util/JsonUtilTest.java
+++ src/test/java/com/ll/standard/util/JsonUtilTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JsonUtilTest {
     @Test
-    @DisplayName("맵을 JSON으로 바꿀 수 있다.")
+    @DisplayName("맵을 JSON으로 바꿀 수 있다.(필드 1개)")
     public void t1() {
         // given
         Map<String, Object> map = new HashMap<>();
@@ -23,6 +23,26 @@ public class JsonUtilTest {
         assertThat(jsonStr).isEqualTo("""
                 {
                     "name": "이름"
+                }
+                """.stripIndent().trim());
+    }
+
+    @Test
+    @DisplayName("맵을 JSON으로 바꿀 수 있다.(필드 2개)")
+    public void t2() {
+        // given
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "이름");
+        map.put("gender", "남자");
+
+        // when
+        String jsonStr = Util.json.toString(map);
+
+        // then
+        assertThat(jsonStr).isEqualTo("""
+                {
+                    "name": "이름",
+                    "gender": "남자"
                 }
                 """.stripIndent().trim());
     }
