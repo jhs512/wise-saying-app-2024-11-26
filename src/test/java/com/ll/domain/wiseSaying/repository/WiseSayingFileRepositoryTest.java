d17e29ccab3fbb18e952e02b01239119ba829fba
jhs512 <jangka512@gmail.com>
2024-11-28 10:42:20 +0900

    f1 b

--- src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
+++ src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,17 +30,11 @@ public class WiseSayingFileRepositoryTest {
         WiseSaying wiseSaying = new WiseSaying(0, "꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
         wiseSayingRepository.save(wiseSaying);
 
-        String filePath = WiseSayingFileRepository.getRowFilePath(wiseSaying.getId());
+        Optional<WiseSaying> opWiseSaying = wiseSayingRepository.findById(wiseSaying.getId());
 
         assertThat(
-                Util.file.exists(filePath)
-        ).isTrue();
-
-        String jsonStr = Util.file.get(filePath, "");
-        Map<String, Object> wiseSayingMap = Util.json.toMap(jsonStr);
-        WiseSaying wiseSayingRestored = new WiseSaying(wiseSayingMap);
-
-        assertThat(wiseSayingRestored).isEqualTo(wiseSaying);
+                opWiseSaying.get()
+        ).isEqualTo(wiseSaying);
     }
 
     @Test
