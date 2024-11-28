15476f49c87cec66769f3c4c5c49d360704a9b5d
jhs512 <jangka512@gmail.com>
2024-11-28 10:37:10 +0900

    f1 r

--- src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
+++ src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -56,5 +57,18 @@ public class WiseSayingFileRepositoryTest {
         assertThat(
                 Util.file.exists(filePath)
         ).isFalse();
+    }
+
+    @Test
+    @DisplayName("명언 단건조회")
+    public void t3() {
+        WiseSaying wiseSaying = new WiseSaying(0, "꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
+        wiseSayingRepository.save(wiseSaying);
+
+        Optional<WiseSaying> opWiseSaying = wiseSayingRepository.findById(wiseSaying.getId());
+
+        assertThat(
+                opWiseSaying.get()
+        ).isEqualTo(wiseSaying);
     }
 }
