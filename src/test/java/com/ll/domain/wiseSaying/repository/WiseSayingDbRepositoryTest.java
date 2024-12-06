f1c517453ec401602239fbabbadac49b298a30a5
jhs512 <jangka512@gmail.com>
2024-12-06 11:30:10 +0900

    db repo : save, findById : r

--- src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingDbRepositoryTest.java
+++ src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingDbRepositoryTest.java
@@ -1,9 +1,15 @@
 package com.ll.domain.wiseSaying.repository;
 
+import com.ll.domain.wiseSaying.entity.WiseSaying;
 import com.ll.global.app.AppConfig;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class WiseSayingDbRepositoryTest {
     private static WiseSayingDbRepository wiseSayingRepository;
@@ -25,5 +31,18 @@ public class WiseSayingDbRepositoryTest {
     @Test
     public void t0() {
 
+    }
+
+    @Test
+    @DisplayName("명언 저장")
+    public void t1() {
+        WiseSaying wiseSaying = new WiseSaying(0, "꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
+        wiseSayingRepository.save(wiseSaying);
+
+        Optional<WiseSaying> opWiseSaying = wiseSayingRepository.findById(wiseSaying.getId());
+
+        assertThat(
+                opWiseSaying.get()
+        ).isEqualTo(wiseSaying);
     }
 }
